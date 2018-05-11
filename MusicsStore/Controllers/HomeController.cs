0d5a93fd0f991cdb139ebc70bb904d0b15ec414e
Abimbola21 <bimzy4me@gmail.com>
2018-05-11 09:26:23 -0500

    Customer Model, Controller, Views added

---
+++ MusicsStore/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MusicsStore.Data;
+using MusicsStore.Models;
+
+namespace MusicsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+   
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Songs.Where(m => m.IsFeatured == true).ToList());
+        }
+        public IActionResult Albums()
+        {
+            return View(_context.Songs.GroupBy(x => x.Album).Select(grp => grp.First()).ToList());
+        }
+        public IActionResult Songs()
+        {
+            return View(_context.Songs.Where(y => y.IsActive == true).ToList());
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MusicsStore/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicsStore.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }    
+        public string Name { get; set; }
+        public int Age { get; set; }
+        [Display(Name = "Favorite Genre")]
+        public string FavGenre { get; set; }
+        [Display(Name = "Favorite Song")]
+        public string FavSong { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        //public override string ToString()
+        //{
+        //    return base.ToString();
+        //return $nameOf
+        //}
+    }
+
+}
